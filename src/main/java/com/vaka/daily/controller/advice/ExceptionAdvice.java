8dd31b696e44adc7d8e3efdeb10f9eccca9b8731
vakaheydev <116391267+vakaheydev@users.noreply.github.com>
2025-02-11 14:42:07 +0300

    Changed in docker files

--- src/main/java/com/vaka/daily/controller/advice/ExceptionAdvice.java
+++ src/main/java/com/vaka/daily/controller/advice/ExceptionAdvice.java
@@ -4,11 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-// @RestControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class ExceptionAdvice {
     @ExceptionHandler(Exception.class)
