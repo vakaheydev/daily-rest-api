99ae530ef4ba6069a81b654abb9627fe8becb3b0
vakaheydev <116391267+vakaheydev@users.noreply.github.com>
2025-03-24 14:48:54 +0300

    Refactored doker files

--- docker/db/db-scripts/re-create.sh
+++ docker/db/db-scripts/re-create.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 # re-create.sh
-psql -U postgres -d vaka_daily_db -f drop.sql
-psql -U postgres -d vaka_daily_db -f create.sql
-psql -U postgres -d vaka_daily_db -f insert.sql
+psql -U postgres -d vaka_daily_db -f /sql-scripts/drop.sql
+psql -U postgres -d vaka_daily_db -f /sql-scripts/create.sql
+psql -U postgres -d vaka_daily_db -f /sql-scripts/insert.sql
--- src/main/java/com/vaka/daily/controller/advice/DataIntegrityViolationExceptionAdvice.java
+++ src/main/java/com/vaka/daily/controller/advice/DataIntegrityViolationExceptionAdvice.java
@@ -7,19 +7,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestControllerAdvice
 @Slf4j
 @Order(1)
 public class DataIntegrityViolationExceptionAdvice {
+    private final Pattern duplicateMsgPattern = Pattern.compile("(\\(.+\\))");
+
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public Map<String, String> methodArgumentTypeMismatchExceptionHandler(DataIntegrityViolationException ex) {
-        log.error("{} | Data integrity exception: {}", ex.getClass().getSimpleName(),
-                ex.getMostSpecificCause().getMessage());
+        String message = ex.getMostSpecificCause().getMessage();
+
+        if (message.contains("duplicate key")) {
+            return resolveDuplicateKeyException(ex);
+        }
+
+        log.error("{} | Data integrity exception: {}", ex.getClass().getSimpleName(), message);
 
         Map<String, String> map = new HashMap<>();
         map.put("error", ex.getClass().getSimpleName());
@@ -27,5 +37,27 @@ public class DataIntegrityViolationExceptionAdvice {
                 String.format("Bad request (data integrity exception): %s", ex.getMostSpecificCause().getMessage()));
         map.put("status", String.valueOf(HttpStatus.CONFLICT.value()));
         return map;
+    }
+
+    private Map<String, String> resolveDuplicateKeyException(DataIntegrityViolationException ex) {
+        String exMsg = ex.getMostSpecificCause().getMessage();
+        log.error("{} | Duplicate: {}", "DuplicateEntityException", exMsg);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error", "DuplicateEntityException");
+        map.put("message", getDuplicateEntityExMessage(exMsg));
+        map.put("status", String.valueOf(HttpStatus.CONFLICT.value()));
+        return map;
+    }
+
+    private String getDuplicateEntityExMessage(String exMsg) {
+        String secondLine = exMsg.split("\n")[1];
+        Matcher matcher = duplicateMsgPattern.matcher(secondLine);
+
+        if (matcher.find()) {
+            return "Duplicates: " + matcher.group(1);
+        }
+
+        throw new IllegalArgumentException("Invalid duplicate key: " + exMsg);
     }
 }
