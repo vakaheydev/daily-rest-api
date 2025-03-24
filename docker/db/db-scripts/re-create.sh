4eff65718cb70b2954ee2c1d854a3bd1580b98d4
vakaheydev <116391267+vakaheydev@users.noreply.github.com>
2025-03-24 15:55:12 +0300

    Refactoring

--- docker/db/db-scripts/re-create.sh
+++ docker/db/db-scripts/re-create.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # re-create.sh
-psql -U postgres -d vaka_daily_db -f /sql-scripts/drop.sql
-psql -U postgres -d vaka_daily_db -f /sql-scripts/create.sql
-psql -U postgres -d vaka_daily_db -f /sql-scripts/insert.sql
+psql -U postgres -d vaka_daily_db -f /sql-scripts/sql/drop.sql
+psql -U postgres -d vaka_daily_db -f /sql-scripts/sql/create.sql
+psql -U postgres -d vaka_daily_db -f /sql-scripts/sql/insert.sql
